da2e926cdc17e9dee9406a3cb07eb9d833dd02fe
Andrey33215 <konstantinov.andrej@yandex.ru>
2017-03-20 12:28:52 +0300

    Add files via upload

---
+++ menudata.js
@@ -0,0 +1,23 @@
+var menudata={children:[
+{text:"Титульная страница",url:"index.html"},
+{text:"Пространства имен",url:"namespaces.html",children:[
+{text:"Пространства имен",url:"namespaces.html"}]},
+{text:"Классы",url:"annotated.html",children:[
+{text:"Классы",url:"annotated.html"},
+{text:"Алфавитный указатель классов",url:"classes.html"},
+{text:"Члены классов",url:"functions.html",children:[
+{text:"Указатель",url:"functions.html",children:[
+{text:"_",url:"functions.html#index__"},
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions.html#index_c"},
+{text:"g",url:"functions.html#index_g"},
+{text:"m",url:"functions.html#index_m"},
+{text:"p",url:"functions.html#index_p"},
+{text:"r",url:"functions.html#index_r"},
+{text:"s",url:"functions.html#index_s"},
+{text:"w",url:"functions.html#index_w"}]},
+{text:"Функции",url:"functions_func.html"},
+{text:"Переменные",url:"functions_vars.html"},
+{text:"Свойства",url:"functions_prop.html"}]}]},
+{text:"Файлы",url:"files.html",children:[
+{text:"Файлы",url:"files.html"}]}]}
